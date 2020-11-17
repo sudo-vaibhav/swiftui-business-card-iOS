fe8df84f32bf757a02c5ac97e56b3ded007d1742
Vaibhav <mailvaibhavchopra@gmail.com>
2020-11-18 02:01:14 +0530

    Initial Commit

---
+++ myBusinessCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  myBusinessCard
+//
+//  Created by Vaibhav Chopra on 18/11/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ myBusinessCard/myBusinessCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  myBusinessCardApp.swift
+//  myBusinessCard
+//
+//  Created by Vaibhav Chopra on 18/11/20.
+//
+
+import SwiftUI
+
+@main
+struct myBusinessCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
